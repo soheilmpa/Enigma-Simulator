f5ab2c363ef4c7bbbe0de6b65799f2cf5539b5ee
Soheil <soheilmohammadpor13@gmail.com>
2021-08-16 23:00:26 +0430

    reset function and some changes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-String = r'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{}[]()<>/\!@#$%^&_-=+* '
+String = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]()<>/!@#$^&_-=+* '
 
-r11 = r'okE(5PW{!wn^3 L/Gi2m]081h=CdA\JQ+eF%Bt9&UxYl6>c-uZs)fr#ypNSgOX7V$IK@M*Tz_4aHD[<Rjqb}v'
-r22 = r'nI 9b1{ChxmOjDSWBJy<7Q6#@cs3V>dlt*80zeuk[]H4)PKRgNM&o_G2va!(pY+T/A$qXf=^%i\E-}5LUZrFw'
-r33 = r'aG(m\UMIqS*t9]0 h$AF%ONR2D_1vd5[^VieP)f8Hw3o/CzKZ-}EL={sQWJr6!7kTpu@ncgx+#jBlYyX><b4&'
+R1 = '[ZuO(/#azs_hiTe]X<vrM)!SAQ^y6I3kFmU$EYj*N> 2l7t&1BR9fgw@5VDJn-K4L8Cq=oGbxpdcHP0W+'
+R2 = 'TMkJXwIZlh5)Wj=uB_sLo1@f*FDHS4Ctr&Q-2RAd0KqmcV>8!GNzaU(/x[ep9+ n6PvYOyb]i3g$<7#E^'
+R3 = 'XoQ9*SDr_1]JGdgZi0w>OheMRq@E2lmzpxv#f7VF4!cLYj38[W($CA5-^sPI+/a)HNKky<6BbnUut =&T'
+R4 = '<4K=f)a(jkCTIHn-g3ov8lqbPXVzNOLc/FmruYJ!yZ[s6d1p25*tB&h]GR_D$9i^>@70 WxQ#wAUS+EeM'
+R5 = '<4K=f)a(jkCTIHn-g3ov8lqbPXVzNOLc/FmruYJ!yZ[s6d1p25*tB&h]GR_D$9i^>@70 WxQ#wAUS+EeM'
 
+# String='ظطزرذدپو./گکمنتالبیسشضصثقفغع هخحجچ'
+# r11 = 'ضصشسظطثقیبزرفغلاذدعهتنپوخحمک./جچ گ'
+# r22 = 'چجحخهعغفقثصضشسیبلاتنمکگ/.وپدذ رزطظ'
+# r33 = 'گکمنتالبیسشضصثقفغعدذرزطظپهوخ. ح/جچ'
+
+print(len(String))
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -35,25 +43,31 @@ class Ui_MainWindow(object):
         c1 = r1[String.find(c)]
         c2 = r2[String.find(c1)]
         c3 = r3[String.find(c2)]
-        reflected = String[25-String.find(c3)]
-        c3 = String[r3.find(reflected)]
+        c4 = r4[String.find(c3)]
+        c5 = r5[String.find(c5)]
+        reflected = String[81-String.find(c5)]
+        c5 = String[r5.find(reflected)]
+        c4 = String[r4.find(c5)]
+        c3 = String[r3.find(c4)]
         c2 = String[r2.find(c3)]
         c1 = String[r1.find(c2)]
         return c1
 
     def rotate(self):
-        global r1 , r2 , r3
+        global r1 , r2 , r3 , r4 , r5
         r1 = r1[1:] + r1[0]
-        if self.state % 26 :
+        if self.state % 15 :
             r2 = r2[1:] + r2[0]
-        if self.state % 676 :
+        if self.state % 50 :
             r3 = r3[1:] + r3[0]
+        if self.state % 200 :
+            r4 = r4[1:] + r4[0]
+        if self.state % 500 :
+            r5 = r5[1:] + r5[0]
 
     def reset(self):
-        global r1 , r2 , r3
-        r1 = r11
-        r2 = r22
-        r3 = r33
+        global r1 , r2 , r3 , r4 , r5
+        r1 , r2 , r3 , r4 , r5 = R1 , R2 , R3 , R4 , R5
         self.state = 0
 
     def convert(self):
